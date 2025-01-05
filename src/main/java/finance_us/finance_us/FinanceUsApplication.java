0cf0563a9557a851f4b331cc270386dcbfbd8066
yuna <micha0215@tukorea.ac.kr>
2025-01-05 16:28:16 +0900

    [chore] 초기세팅

---
+++ src/main/java/finance_us/finance_us/FinanceUsApplication.java
@@ -0,0 +1,13 @@
+package finance_us.finance_us;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceUsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceUsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/finance_us/finance_us/FinanceUsApplicationTests.java
@@ -0,0 +1,13 @@
+package finance_us.finance_us;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceUsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
