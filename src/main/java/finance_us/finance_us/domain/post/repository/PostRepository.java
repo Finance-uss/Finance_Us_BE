fc4721e734bd7875404b518f6de35e6bc2762a07
InSooBeen <isb8180@naver.com>
2025-01-30 17:00:31 +0900

    [#45] Feat : 키워드 기반 게시글 조회 쿼리 구현

--- src/main/java/finance_us/finance_us/domain/post/repository/PostRepository.java
+++ src/main/java/finance_us/finance_us/domain/post/repository/PostRepository.java
@@ -1,6 +1,8 @@
 package finance_us.finance_us.domain.post.repository;
 
 import finance_us.finance_us.domain.post.entity.Post;
+import finance_us.finance_us.domain.post.entity.status.PostType;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -22,5 +24,13 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
     @Query(value="select p.* from post_scrap ps inner join post p on p.id=ps.post_id where ps.user_id=:userId ;", nativeQuery=true)
     public List<Post> findByUserScraped(@Param("userId") Long userId);
+
+    //게시판 유형, 키워드를 통한 조회
+    @Query("SELECT p FROM Post p WHERE p.postType = :boardType AND p.id > :lastId And (p.title LIKE %:keyword% OR p.content LIKE %:keyword%) ORDER BY p.id ASC")
+    List<Post> findByCategoryAndKeywordWithPaging(
+            @Param("boardType") PostType boardType,
+            @Param("lastId") Long lastId,
+            @Param("keyword") String keyword,
+            Pageable pageable);
 
 }
