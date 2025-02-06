bdae9a3dbd5bea215ed6411cce1a07c4e66da8a0
@LYST <101388967+gabury1@users.noreply.github.com>
2025-02-06 23:33:52 +0900

    서버 세팅

--- src/main/java/finance_us/finance_us/global/config/WebSecurityConfig.java
+++ src/main/java/finance_us/finance_us/global/config/WebSecurityConfig.java
@@ -91,6 +91,11 @@ public class WebSecurityConfig {
         config.setMaxAge(MAX_AGE_SECS);
         config.addAllowedOrigin("http://localhost:8080");
         config.addAllowedOrigin("ws://localhost:8080");
+        config.addAllowedOrigin("http://13.209.210.46:8080");
+        config.addAllowedOrigin("ws://13.209.210.46:8080");
+
+        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("http://localhost:5173");
         source.registerCorsConfiguration("/**", config);
         return source;
     }
