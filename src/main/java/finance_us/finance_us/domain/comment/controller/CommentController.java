3f83ce9065a307eab16354f66622188cb2739bfb
Jihyun <parkjh2910@gmail.com>
2025-02-16 17:07:06 +0900

    [#108] Refactor : 커뮤니티 API 요약 설명 작성

--- src/main/java/finance_us/finance_us/domain/comment/controller/CommentController.java
+++ src/main/java/finance_us/finance_us/domain/comment/controller/CommentController.java
@@ -6,6 +6,8 @@ import finance_us.finance_us.domain.comment.dto.CommentResponse;
 import finance_us.finance_us.domain.comment.entity.Comment;
 import finance_us.finance_us.domain.comment.service.CommentService;
 import finance_us.finance_us.global.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
@@ -13,11 +15,13 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/comment")
+@Tag(name = "Comment API", description = "댓글 관련 API")
 public class CommentController {
     private final CommentService commentService;
 
     // 댓글 작성
     @PostMapping("/{postId}")
+    @Operation(summary = "댓글 작성 API")
     public ApiResponse<CommentResponse.CommentResponseDTO> createComment(@RequestHeader("Authorization") String token, @PathVariable Long postId, @RequestBody CommentRequest.CommentRequestDTO request) {
         Comment comment = commentService.createComment(token, postId, request);
         return ApiResponse.onSuccess(CommentConverter.toCommentResponseDTO(comment));
@@ -25,6 +29,7 @@ public class CommentController {
 
     // 댓글 수정
     @PatchMapping("/{commentId}")
+    @Operation(summary = "댓글 수정 API")
     public ApiResponse<CommentResponse.CommentResponseDTO> updateComment(@RequestHeader("Authorization") String token, @PathVariable Long commentId, @RequestBody CommentRequest.CommentUpdateDTO request) {
         Comment comment = commentService.updateComment(token, commentId, request);
         return ApiResponse.onSuccess(CommentConverter.toCommentResponseDTO(comment));
@@ -32,6 +37,7 @@ public class CommentController {
 
     // 댓글 삭제
     @DeleteMapping("/{commentId}")
+    @Operation(summary = "댓글 삭제 API")
     public ApiResponse<Boolean> deleteComment(@RequestHeader("Authorization") String token, @PathVariable Long commentId) {
         commentService.deleteComment(token, commentId);
         return ApiResponse.onSuccess(true);
@@ -39,6 +45,7 @@ public class CommentController {
 
     // 댓글 갯수 및 목록 반환
     @GetMapping("/{postId}")
+    @Operation(summary = "댓글 갯수 및 목록 조회 API")
     public ApiResponse<CommentResponse.CommentResultDTO> getCommentsByPost(@RequestHeader("Authorization") String token, @PathVariable Long postId) {
         List<CommentResponse.CommentDTO> commentsList = commentService.getCommentsByPostWithReplies(token, postId);
         int commentCount = commentService.getCommentCount(token, postId);
