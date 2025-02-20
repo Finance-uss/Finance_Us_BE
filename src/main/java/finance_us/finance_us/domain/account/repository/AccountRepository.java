b3ff95fdff4a02f904a37c4652fd2319db75925e
yuna <micha0215@tukorea.ac.kr>
2025-02-20 16:51:08 +0900

    [#130] refactor: 가계부-팔로우 목표금액 퍼센트 수정

--- src/main/java/finance_us/finance_us/domain/account/repository/AccountRepository.java
+++ src/main/java/finance_us/finance_us/domain/account/repository/AccountRepository.java
@@ -41,6 +41,7 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
 
     // followingId로 데이터 조회
     @Query("SELECT a FROM Account a WHERE a.user.Id = :followingId AND a.status = true " +
+            "AND a.accountType = 'expense' " +
             "AND FUNCTION('YEAR', a.date) = :year " +
             "AND FUNCTION('MONTH', a.date) = :month " +
             "ORDER BY a.date DESC")
--- src/main/java/finance_us/finance_us/domain/category/entity/SubCategory.java
+++ src/main/java/finance_us/finance_us/domain/category/entity/SubCategory.java
@@ -1,11 +1,14 @@
 package finance_us.finance_us.domain.category.entity;
 
+import finance_us.finance_us.domain.account.entity.Account;
 import finance_us.finance_us.domain.common.entity.BaseEntity;
 import finance_us.finance_us.domain.user.entity.User;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
+
+import java.util.List;
 
 @Entity
 @Getter
@@ -33,5 +36,9 @@ public class SubCategory extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "main_category_id")
     private MainCategory mainCategory;
+
+
+    @OneToMany(mappedBy = "subCategory")
+    private List<Account> accounts;
 }
 
