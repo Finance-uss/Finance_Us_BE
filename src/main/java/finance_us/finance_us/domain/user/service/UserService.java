504ab1f808f9ab2008957a46a31fb5f872238225
haeun <103066456+haeun9634@users.noreply.github.com>
2025-02-20 20:49:45 +0900

    Refactor: 사용자 탈퇴 기능 수정

--- src/main/java/finance_us/finance_us/domain/user/service/UserService.java
+++ src/main/java/finance_us/finance_us/domain/user/service/UserService.java
@@ -165,9 +165,7 @@ public class UserService {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new GeneralException(ErrorStatus.MEMBER_NOT_FOUND));
         user.setName("탈퇴한 사용자입니다.");
-        user.setAge(null);
         user.setOne_liner(null);
-        user.setJob(null);
 user.setEmail(null);
      user.setPassword(null); 
 user.setImageName(null);
